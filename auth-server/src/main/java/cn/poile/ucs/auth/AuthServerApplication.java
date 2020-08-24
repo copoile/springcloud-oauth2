ec465fb55c3353dcc73c520bea32fa32d444fda5
yahow <726856005@qq.com>
2020-08-24 22:27:34 +0800

    unimport

--- auth-server/src/main/java/cn/poile/ucs/auth/AuthServerApplication.java
+++ auth-server/src/main/java/cn/poile/ucs/auth/AuthServerApplication.java
@@ -1,14 +1,10 @@
 package cn.poile.ucs.auth;
-import cn.poile.ucs.auth.config.IgnoreLogoutFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 
-import javax.servlet.Filter;
 
 
 /**
